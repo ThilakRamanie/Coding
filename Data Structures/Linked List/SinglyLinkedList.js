e2d6990b7e4454cfbd5947caa26a63e17ea2fc3f
ThilakRamanie <thilakramanie007@gmail.com>
2024-02-02 21:34:19 +0000

    Singly linked list -- GET,SET,SHIFT,UNSHIFT functions

--- Data Structures/Linked List/SinglyLinkedList.js
+++ Data Structures/Linked List/SinglyLinkedList.js
@@ -49,6 +49,46 @@ class SinglyLinkedList {
         }
         return currentHead;
     }
+    shift() {
+        if(!this.head) {
+            return undefined;
+        }
+        const tempHead = this.head;
+        this.head = this.head.next;
+        tempHead.next = null;
+        this.length--;
+        if(this.length===0) {
+            this.tail = null;
+        }
+        return tempHead;
+    }
+    unshift(val) {
+        const newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else {
+            newNode.next = this.head;
+        this.head = newNode;
+        }
+        this.length++;
+    }
+    get(index) {
+        if(this.length <= index || index<0) return false;
+        let currentELement=this.head;
+        for(let i=0;i<index;i++) {
+            currentELement = currentELement.next;
+        }
+        return true;
+    }
+    set(index, value) {
+        const getNode = this.get(index);
+        if(!getNode) {
+            return false;
+        }
+        getNode.val = value;
+    }
 }
 
 var list = new SinglyLinkedList();
@@ -56,7 +96,9 @@ list.push(5);
 list.push(10);
 list.push(15);
 list.push(20);
+console.log(list);
 list.traverse();
-list.pop();
-console.log(list)
+list.get(1);
+list.set(1,'Love');
+list.set(2,'you');
 list.traverse();
